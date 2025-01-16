547eba2fad2056081a8d775e5e0daaf42dafed97
Vivid-Vortex <ctntc9.dpk@gmail.com>
2025-01-16 18:10:07 +0530

    current date

--- CommandsAndShortcuts/Google/Sheet/Scripts/GenerateSeperateReportSheet.js
+++ CommandsAndShortcuts/Google/Sheet/Scripts/GenerateSeperateReportSheet.js
@@ -33,7 +33,8 @@ function sumAndInsertUntilColumn() {
   let currentDate = new Date(firstDate);
 
   // Loop through each week (every 6 columns) until March 31st (stopColumn)
-  while (currentDate <= new Date('2025-12-31')) {
+  // while (currentDate <= new Date('2025-12-31')) { / Get report generated for all year
+  while (currentDate <= new Date()) { // Get report generated till current date
     console.log("Loop Starts --------------------------------------->>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>")
     
     // Get the start of the week (Monday) and the end of the week (Sunday)
